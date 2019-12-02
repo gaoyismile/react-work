27e877a429cc292748227b14ac0d59af360f5a49
flowgao <634664216@qq.com>
2019-12-02 14:47:56 +0800

    初始化tree数据,并调用接口

--- src/pages/account/settings/components/RalateMan/components/Add.js
+++ src/pages/account/settings/components/RalateMan/components/Add.js
@@ -4,150 +4,16 @@ import Tree from './tree/Tree';
 import treeData from './tree.json';
 import styles from './style.less';
 import { GridContent } from '@ant-design/pro-layout';
+import { Dispatch, Action } from 'redux';
+import { FormComponentProps } from 'antd/es/form';
+
 import Userlist from './Userlist';
 
 class TreeComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      objectData: {
-        id: 2,
-        name: '1#联合调试队',
-        pid: 1,
-        children: [
-          {
-            id: 3,
-            name: '队部',
-            pid: 2,
-            children: [],
-          },
-          {
-            id: 4,
-            name: '质量安全部',
-            pid: 2,
-            children: [
-              {
-                id: 5,
-                name: '质量控制科',
-                pid: 4,
-                children: [],
-              },
-              {
-                id: 6,
-                name: '安全科',
-                pid: 4,
-                children: [],
-              },
-            ],
-          },
-          {
-            id: 7,
-            name: '工程部',
-            pid: 2,
-            children: [
-              {
-                id: 8,
-                name: '进度科',
-                pid: 7,
-                children: [],
-              },
-              {
-                id: 9,
-                name: '移交接产科',
-                pid: 7,
-                children: [],
-              },
-              {
-                id: 10,
-                name: '综合管理科',
-                pid: 7,
-                children: [],
-              },
-            ],
-          },
-          {
-            id: 11,
-            name: '技术部',
-            pid: 2,
-            children: [
-              {
-                id: 12,
-                name: '调试隔离办',
-                pid: 11,
-                children: [],
-              },
-              {
-                id: 13,
-                name: '技术管理科',
-                pid: 11,
-                children: [],
-              },
-            ],
-          },
-          {
-            id: 14,
-            name: '核岛工艺队',
-            pid: 2,
-            children: [
-              {
-                id: 15,
-                name: '核岛主系统科',
-                pid: 14,
-                children: [],
-              },
-              {
-                id: 16,
-                name: '核岛辅助系统科',
-                pid: 14,
-                children: [],
-              },
-              {
-                id: 17,
-                name: '三废系统科',
-                pid: 14,
-                children: [],
-              },
-            ],
-          },
-          {
-            id: 18,
-            name: '技术支持队',
-            pid: 2,
-            children: [
-              {
-                id: 19,
-                name: '维修管理科',
-                pid: 18,
-                children: [],
-              },
-              {
-                id: 20,
-                name: '疫检材料科',
-                pid: 18,
-                children: [],
-              },
-              {
-                id: 21,
-                name: '性能试验科',
-                pid: 18,
-                children: [],
-              },
-              {
-                id: 22,
-                name: '物理热工科',
-                pid: 18,
-                children: [],
-              },
-            ],
-          },
-          {
-            id: 23,
-            name: '联合调试一处',
-            pid: 2,
-            children: [],
-          },
-        ],
-      },
+      objectData: {},
       deptId: null,
       loading: false,
       userName: '',
@@ -155,6 +21,25 @@ class TreeComponent extends React.Component {
     };
   }
 
+  componentDidMount() {
+    console.log("初始化---------------------");
+    var t = this;
+    fetch("/api/getTreeJson", {method: 'GET'}).then(
+      function (res) {
+          res.json().then(function (data) {
+                  var dataJson = JSON.stringify(data.objectData);
+                  var datas = dataJson.substring(1,dataJson.length-1);
+                  console.log(datas);
+                  t.setState({
+                    //objectData:{"id":1,"name":"工作管理平台","pid":0,"children":[{"id":2,"name":"1#联合调试队","pid":1,"children":[{"id":3,"name":"队部","pid":2,"children":[]},{"id":4,"name":"质量安全部","pid":2,"children":[{"id":5,"name":"质量控制科","pid":4,"children":[]},{"id":6,"name":"安全科","pid":4,"children":[]}]},{"id":7,"name":"工程部","pid":2,"children":[{"id":8,"name":"进度科","pid":7,"children":[]},{"id":9,"name":"移交接产科","pid":7,"children":[]},{"id":10,"name":"综合管理科","pid":7,"children":[]}]},{"id":11,"name":"技术部","pid":2,"children":[{"id":12,"name":"调试隔离办","pid":11,"children":[]},{"id":13,"name":"技术管理科","pid":11,"children":[]}]},{"id":14,"name":"核岛工艺队","pid":2,"children":[{"id":15,"name":"核岛主系统科","pid":14,"children":[]},{"id":16,"name":"核岛辅助系统科","pid":14,"children":[]},{"id":17,"name":"三废系统科","pid":14,"children":[]}]},{"id":18,"name":"技术支持队","pid":2,"children":[{"id":19,"name":"维修管理科","pid":18,"children":[]},{"id":20,"name":"疫检材料科","pid":18,"children":[]},{"id":21,"name":"性能试验科","pid":18,"children":[]},{"id":22,"name":"物理热工科","pid":18,"children":[]}]},{"id":23,"name":"联合调试一处","pid":2,"children":[]}]},{"id":24,"name":"2#联合调试队","pid":1,"children":[{"id":25,"name":"质量安全部","pid":24,"children":[]},{"id":26,"name":"队部","pid":24,"children":[]}]}]}
+                    objectData: JSON.parse(datas)
+                  });
+              }
+          )
+      });
+      
+  }
+  
   renderChildren = () => {
     return <Userlist userName={this.state.names} />;
   };
@@ -163,6 +48,7 @@ class TreeComponent extends React.Component {
     console.info('TreeTest---onNodeClick---', node);
     this.setState({ deptId: node.id });
     console.log('this.state.names', node.name);
+
   };
 
   setValue(event) {
@@ -215,23 +101,12 @@ class TreeComponent extends React.Component {
             function(strs, index) {
               return (
                 <Col lg={4} md={24}>
-                  <div onClick={this.removeArray.bind(this, index)}>{strs}</div>
+                  <div className={styles.removeDiv} onClick={this.removeArray.bind(this, index)}>{strs}</div>
                 </Col>
               );
             }.bind(this),
           )}
-
-          {/* {this.state.resultArray} */}
         </Row>
-        {/* <div className={styles.total}>
-          <div className={styles.leftMenu}>
-            
-          </div>
-          <div className={styles.right}>
-            <Userlist key={new Date()} deptId={this.state.deptId} />
-          </div>
-          666
-        </div> */}
       </GridContent>
     );
   }
